ada003977f60de1c4b084614e7f68acbd16d85c7
n.lebedev.2019 <nikitalebedeff@gmail.com>
2021-12-08 23:49:45 +0700

    + heroku стили

--- djangoCS/settings.py
+++ djangoCS/settings.py
@@ -16,7 +16,9 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.conf.urls import static
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -129,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
