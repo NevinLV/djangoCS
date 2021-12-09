a1cef91115da8774f58bfbf6aea1e1650c8705c5
n.lebedev.2019 <nikitalebedeff@gmail.com>
2021-12-09 20:05:01 +0700

    API + author and category

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -6,4 +6,14 @@ from main.models import *
 class CoursesSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Courses
-        fields = ('title', 'description', 'date', 'category_id', 'author_id')
+        fields = ('title', 'description', 'date', 'category', 'author')
+
+    author = serializers.SerializerMethodField('get_author_name')
+
+    def get_author_name(self, obj):
+        return obj.author.name
+
+    category = serializers.SerializerMethodField('get_categ_title')
+
+    def get_categ_title(self, obj):
+        return obj.category.title
--- myapi/views.py
+++ myapi/views.py
@@ -4,6 +4,7 @@ from main.models import Courses
 from rest_framework import viewsets
 from myapi.serializers import CoursesSerializer
 
+
 class CoursesViewSet(viewsets.ModelViewSet):
     queryset = Courses.objects.all()
     serializer_class = CoursesSerializer
