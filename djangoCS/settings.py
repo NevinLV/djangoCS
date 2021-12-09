84baeb41a46bd764f7139cf215d33eca59f9341d
n.lebedev.2019 <nikitalebedeff@gmail.com>
2021-12-09 23:31:10 +0700

    test

--- djangoCS/settings.py
+++ djangoCS/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
