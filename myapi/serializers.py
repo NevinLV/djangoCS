aaa7b84f4f323a62625f0beea4d6ea40c7425ab1
n.lebedev.2019 <nikitalebedeff@gmail.com>
2021-12-16 00:26:47 +0700

    + Создание Курсов 3

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -17,3 +17,18 @@ class CoursesSerializer(serializers.HyperlinkedModelSerializer):
 
     def get_categ_title(self, obj):
         return obj.category.title
+
+class CoursesSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Courses
+        fields = ('title', 'description', 'date', 'category', 'author')
+
+    author = serializers.SerializerMethodField('get_author_name')
+
+    def get_author_name(self, obj):
+        return obj.author.name
+
+    category = serializers.SerializerMethodField('get_categ_title')
+
+    def get_categ_title(self, obj):
+        return obj.category.title
